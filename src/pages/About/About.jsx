b4014bf670907635bc9e0ac2b21ef5ccab01e1ba
Jagruti <104372611+jagrutijaggu@users.noreply.github.com>
2025-04-17 22:23:55 +0530

    fix: correct spelling of 'BooksHaven' in About component

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -14,10 +14,10 @@ const About = () => {
             <img src = {aboutImg} alt = "" />
           </div>
           <div className='about-text'>
-            <h2 className='about-title fs-26 ls-1'>About BookHub</h2>
-            <p className='fs-17'>Books Haven is an open book library where anyone can read, share, and explore a world of knowledge—completely free. Whether you're into classic literature, modern fiction, or niche nonfiction, we’ve got shelves full of stories and ideas waiting for you.</p>
+            <h2 className='about-title fs-26 ls-1'>About BooksHaven</h2>
+            <p className='fs-17'>BooksHaven is an open book library where anyone can read, share, and explore a world of knowledge—completely free. Whether you're into classic literature, modern fiction, or niche nonfiction, we’ve got shelves full of stories and ideas waiting for you.</p>
             <p className='fs-17'>We believe books should be accessible to everyone. That’s why our collection is open to the public, with no membership fees or hidden costs. Just walk in, grab a book, and start reading—or take it home and return it when you’re done.</p>
-            <p className='fs-17'>Books Haven is built by readers, for readers. Come find your next favorite book.</p>
+            <p className='fs-17'>BooksHaven is built by readers, for readers. Come find your next favorite book.</p>
           </div>
         </div>
       </div>
